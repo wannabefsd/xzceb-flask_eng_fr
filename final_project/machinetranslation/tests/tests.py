d2106fc0813bc5adff95c602a13e973255a4f78c
wannabefsd <etienneuae@gmail.com>
2021-11-17 06:39:52 +0000

    final changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,17 @@
+"""method-docstring"""
+import unittest
+
+from translator import english_to_french
+
+class test_english_to_french(unittest.TestCase):
+    """class docsting"""
+    def test1(self):
+        #self.assertEqual(english_to_french(0), 0)
+        self.assertEqual(english_to_french("Hello"), "Bonjour")
+from translator import french_to_english
+class test_french_to_english(unittest.TestCase):
+    """class docstring"""
+    def test1(self):
+        #self.assertEqual(french_to_english(0), 0)
+        self.assertEqual(french_to_english("Bonjour"), "Hello")
+unittest.main()
---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,29 @@
+"""translator"""
+import os
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator('Gx0eHjsabQr-t-WxK-JSB0CCYkF5jkmey4101F1_i99B')
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url('https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/825b5f05-e961-4dd6-9d99-9f09fcad3869')
+
+def english_to_french(english_text):
+    """english to french"""
+    fr_translation=language_translator.translate(text=english_text, model_id="en-fr").get_result()
+    return fr_translation.get("translations")[0].get("translation")
+def french_to_english(french_text):
+    """french to english"""
+    en_translation=language_translator.translate(text=french_text, model_id="fr-en").get_result()
+    return en_translation.get("translations")[0].get("translation")
