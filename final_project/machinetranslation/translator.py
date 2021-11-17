c5e6cfeebf787ec64ce665c5a29b745f4712aff7
wannabefsd <etienneuae@gmail.com>
2021-11-17 10:56:14 +0000

    final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -19,16 +19,16 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url('https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/825b5f05-e961-4dd6-9d99-9f09fcad3869')
 
-def english_to_french(english_text):
+def english_to_french(englishText):
     """english to french"""
-    translation = language_translator.translate(text=english_text, model_id='en-fr').get_result()
-    french_text=translation['translations'][0]['translation']
+    translation = language_translator.translate(text=englishText, model_id='en-fr').get_result()
+    frenchText=translation['translations'][0]['translation']
     print(translation)
-    return french_text
+    return frenchText
     
-def french_to_english(french_text):
+def french_to_english(frenchText):
     """french to english"""
-    translation = language_translator.translate(text=french_text, model_id='fr-en').get_result()
-    english_text=translation['translations'][0]['translation']
+    translation = language_translator.translate(text=frenchText, model_id='fr-en').get_result()
+    englishText=translation['translations'][0]['translation']
     print(translation)
-    return english_text
+    return englishText
