8962883d10128b19fe31c84fb27a0fe088145304
wannabefsd <etienneuae@gmail.com>
2021-11-17 08:22:53 +0000

    final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,13 +1,12 @@
 """method-docstring"""
 import unittest
-
 from translator import english_to_french
 
 class test_english_to_french(unittest.TestCase):
     """class docsting"""
     def test1(self):
-        self.assertEqual(english_to_french('none'), 'null input')
-        self.assertEqual(english_to_french("Hello"), "Bonjour")
+        self.assertEqual(english_to_french(0), 0)
+        self.assertEqual(english_to_french(Hello), Bonjour)
 
 from translator import french_to_english
 
@@ -15,6 +14,6 @@ class test_french_to_english(unittest.TestCase):
     """class docstring"""
     def test1(self):
         self.assertEqual(french_to_english(0), 0)
-        self.assertEqual(french_to_english("Bonjour"), "Hello")
+        self.assertEqual(french_to_english(Bonjour), Hello)
 
 unittest.main()
